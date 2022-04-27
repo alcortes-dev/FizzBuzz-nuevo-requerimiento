47ade89a49a57b1cd4992edb4ec05f65e80b3e49
alcortes <alejandro.alfa3037@gmail.com>
2022-04-26 22:58:18 -0500

    Agregando pruebas para FizzbuzzService

--- test/lib/services/FizzBuzzService.test.js
+++ test/lib/services/FizzBuzzService.test.js
@@ -5,7 +5,7 @@ const explorer2 = {name: "Explorer1", score: 3};
 const explorer3 = {name: "Explorer1", score: 5};
 const explorer4 = {name: "Explorer1", score: 15};
 
-describe("Pruebas para evaluar la funcionalidad FizzBuzz", () => {
+describe("Pruebas para evaluar la funcionalidad FizzBuzz en Explorer", () => {
     test("Validación para valores 3",() => {
         FizzBuzz.applyValidationInExplorer(explorer2);
         expect(explorer2.trick).toBe("FIZZ");
@@ -24,6 +24,30 @@ describe("Pruebas para evaluar la funcionalidad FizzBuzz", () => {
     test("Validación para valores no divisibles por 3 y/o 5",() => {
         FizzBuzz.applyValidationInExplorer(explorer1);
         expect(explorer1.trick).toBe(explorer1.score);
+    });
+
+
+});
+
+describe("Pruebas para evaluar la funcionalidad FizzBuzz en Número", () => {
+    test("Validación para valores 3",() => {
+        const resultado = FizzBuzz.applyValidationInNumber(3);
+        expect(resultado).toBe("FIZZ");
+    });
+
+    test("Validación para valores 5",() => {
+        const resultado = FizzBuzz.applyValidationInNumber(10);
+        expect(resultado).toBe("BUZZ");
+    });
+
+    test("Validación para valores 15",() => {
+        const resultado = FizzBuzz.applyValidationInNumber(30);
+        expect(resultado).toBe("FIZZBUZZ");
+    });
+
+    test("Validación para valores no divisibles por 3 y/o 5",() => {
+        const resultado = FizzBuzz.applyValidationInNumber(2);
+        expect(resultado).toBe(2);
     });
 
 
